f4671f03556ce323c4b71dff1e7d39f9cb460cb8
Waldo Ancacoy <wancacoy2014@alu.uct.cl>
2017-09-02 15:41:52 -0300

    index, portal, ingreso
    un nada con el login

---
+++ taller2/index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>EducaAdmin</title>
+  <!-- ################################# -->
+	<link rel="stylesheet" href="./assets/css/bootstrap.css" media="screen">
+	<link rel="stylesheet" href="./assets/css/bootstrap.min.css" media="screen">
+	<link rel="stylesheet" href="./assets/css/style_x.css">
+	<!-- ################################# -->
+	<!-- ################################# -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+</head>
+<body>
+	<nav class='navbar navbar-toggleable-md navbar-inverse bg-inverse'>
+  <button class='navbar-toggler navbar-toggler-right collapsed' type='button' data-toggle='collapse' data-target='#Tg-w02' aria-controls='Tg-w02' aria-expanded='false' aria-label='Toggle navigation'>
+    <span class='navbar-toggler-icon'></span>
+  </button>
+  <div>
+  <a class='navbar-brand' href='#'><img src='./assets/image/logo.png' width='40' height='40'></a>
+  </div>
+  <div class='collapse navbar-collapse' id='Tg-w02'>
+    <ul class='navbar-nav mr-auto mt-2 mt-md-0'>
+      <li class='nav-item'>
+        <a class='nav-link' href='#'>¿Quienes Somos?</a>
+      </li>
+      <li class='nav-item'>
+        <a class='nav-link' href='#'>Sobre el Servicio</a>
+      </li>
+      <li class='nav-item'>
+        <a class='nav-link' href='#'>FAQ</a>
+      </li>
+    </ul>
+    <div class='form-inline my-2 my-lg-0 mr-lg-3'>
+      <div class='dropdown'>
+        <a class="btn btn-outline-info" href="#" role="button">
+        	Solictar Registro
+        </a>
+      </div>
+    </div>
+    <div class='form-inline my-2 my-lg-0'>
+      <div class='dropdown'>
+        <a class="btn btn-outline-success" href="./portal.php" role="button">
+        	Ingresar
+        </a>
+      </div>
+    </div>
+  </div>
+  </nav>
+  <div class="container ">
+  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner" role="listbox">
+    <div class="carousel-item active">
+      <img id="w-img-wh" class="d-block img-fluid" src="https://vignette4.wikia.nocookie.net/bokunoheroacademia/images/e/ec/Anime_Slider_2.png/revision/latest/scale-to-width-down/636?cb=20170323190823" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img id="w-img-wh" class="d-block img-fluid" src="https://inmanga.com/page/getPageImage/?identification=23edd9ba-c2a6-49d8-92c5-c276c859f168-194943" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img id="w-img-wh" class="d-block img-fluid" src="http://www.locosxlosjuegos.com/wp-content/uploads/2017/04/rPMs7Vt.jpg" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+	</div>
+	</div>
+
+	
+	<?php include 'temple/footer.php'; ?>
+  <footer class='container-fluid bg-inverse py-3 mt-2'>
+    <div class='row'>
+      <div class=' col-lg-8 col-lg-offset-2'>
+        <div class='col-sm-4'>
+          <div class='footer-col-item'>
+            <kbd><h4>Contactanos:</h4>
+            <address>
+            6969, Av. Shupa el Perro (WaU).-<br>
+            400015 Temuco, Chile.-
+            </address></kbd>
+          </div>
+        </div>
+      </div>
+      <div class='col-xs-12 '>
+        <kbd><h5>Siguenos y Infomate mas <br/>del Servicio:</h5></kbd>
+          <ul class='social-network social-circle'>
+            <li><a href='#' class='icoRss' title='Rss'><i class='fa fa-rss'></i></a></li>
+            <li><a href='#' class='icoFacebook' title='Facebook'><i class='fa fa-facebook'></i></a></li>
+            <li><a href='#' class='icoTwitter' title='Twitter'><i class='fa fa-twitter'></i></a></li>
+            <li><a href='#' class='icoGoogle' title='Google +'><i class='fa fa-google-plus'></i></a></li>
+            <li><a href='#' class='icoLinkedin' title='Linkedin'><i class='fa fa-linkedin'></i></a></li>
+          </ul>       
+        </div>
+      </div>
+    </div>
+  </footer>
+	<script src="./assets/js/jquery/jquery-3.1.1.min.js"></script>
+  <script src="./assets/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ taller2/portal.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>EducaAdmin</title>
+  <!-- ################################# -->
+	<link rel="stylesheet" href="./assets/css/bootstrap.css" media="screen">
+	<link rel="stylesheet" href="./assets/css/bootstrap.min.css" media="screen">
+	<link rel="stylesheet" href="./assets/css/style_x.css">
+	<!-- ################################# -->
+	<!-- ################################# -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+</head>
+<body>
+	
+  <nav class='navbar navbar-toggleable-md navbar-inverse bg-inverse'>
+  <button class='navbar-toggler navbar-toggler-right collapsed' type='button' data-toggle='collapse' data-target='#Tg-w02' aria-controls='Tg-w02' aria-expanded='false' aria-label='Toggle navigation'>
+    <span class='navbar-toggler-icon'></span>
+  </button>
+  <div>
+  <a class='navbar-brand' href='#'><img src='./assets/image/logo.png' width='40' height='40'></a>
+  </div>
+  <div class='collapse navbar-collapse' id='Tg-w02'>
+    <ul class='navbar-nav mr-auto mt-2 mt-md-0'>
+      <li class='nav-item active'>
+        <a class='nav-link' href='#'>Home</a>
+      </li>
+      <li class='nav-item'>
+        <a class='nav-link' href='#'>Link</a>
+      </li>
+      <li class='nav-item'>
+        <a class='nav-link disabled' href='#'>Disabled</a>
+      </li>
+    </ul>
+
+    <div class='form-inline my-2 my-lg-0'>
+      <div class='dropdown'>
+        <button class='btn btn-secondary dropdown-toggle bg-success' type='button' id='dropdownMenuButton' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
+          Usuario Ingresado
+        </button>
+        <div class='dropdown-menu' aria-labelledby='dropdownMenuButton'>
+          <a class='dropdown-item' href='#'>Cerrar Sesion</a>
+          <a class='dropdown-item' href='#'>Cambiar Contraseña</a>
+          <a class='dropdown-item' href='#'>Otra cosa</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  </nav>
+
+	<div class="w_1 row text-center">
+    <div class="col-6 col-sm-3 placeholder">
+      <a href="#"><img id="w-bor" src="./assets/image/liqui.png" width="200" height="200" class="img-fluid rounded-circle " alt="Generic placeholder thumbnail"></a>
+      <h4>Liquidación</h4>
+      <div class="text-muted">Something else</div>
+    </div>
+    <div class="col-6 col-sm-3 placeholder">
+      <a href="#"><img id="w-bor"  src="./assets/image/matri.png" width="200" height="200" class="img-fluid rounded-circle" alt="Generic placeholder thumbnail"></a>
+      <h4>Matriculas</h4>
+      <span class="text-muted">Something else</span>
+    </div>
+    <div class="col-6 col-sm-3 placeholder">
+      <a href="#"><img id="w-bor" src="./assets/image/o2.png" width="200" height="200" class="img-fluid rounded-circle view hm-zoom" alt="Generic placeholder thumbnail"></a>
+      <h4>RR.HH.</h4>
+      <span class="text-muted">Something else</span>
+    </div>
+    <div class="col-6 col-sm-3 placeholder">
+      <a href="#"><img id="w-bor" src="./assets/image/nota.png" width="200" height="200" class="img-fluid rounded-circle" alt="Generic placeholder thumbnail"></a>
+      <h4>Notas</h4>
+      <span class="text-muted">Something else</span>
+    </div>
+  </div>
+
+
+  <script src="./assets/js/jquery/jquery-3.1.1.min.js"></script>
+  <script src="./assets/js/bootstrap/bootstrap.min.js"></script>
+</body>
+</html>
