6d18320c03176260525c96701930d1051c532232
Alexmzc <michelmz90@hotmail.com>
2017-09-06 01:46:40 -0300

    DESDE 0 - BASE FUNCIONAL LARAVEL
    Base para el trabajo. Contiene:
    - Ejemplos de vistas.
    - Autenticación por default.
    - Maqueteo del panel.
    
    Falta por completar:
    - Mostrar operaciones CRUD.
    - Modelo para las liquidaciones.
    - Migración para las liquidaciones.
    - Controlador para el CRUD.

---
+++ educadmin/resources/views/layouts/container.blade.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/container.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+      <a class="navbar-brand" href="{{ url('/home') }}">Dashboard</a>
+      <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Settings</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Profile</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Help</a>
+          </li>
+        </ul>
+        <form class="form-inline mt-2 mt-md-0">
+          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+  
+   <div class="container-fluid">
+      <div class="row">
+        <nav class="col-sm-3 col-md-2 d-none d-sm-block bg-light sidebar">
+          @yield('navbar')
+        </nav>
+      
+        <main class="col-sm-9 ml-sm-auto col-md-10 pt-3" role="main">
+        @section('panel') 
+          <h1>Panel de administración</h1>
+       
+          <section class="row text-center placeholders">
+            <div class="col-6 col-sm-3 placeholder">
+              <a href="{{ url('/liquidaciones') }}"><img src="{{ asset('images/liquidaciones.png') }}" width="200" height="200" class="img-fluid rounded-circle" alt="Liquidaciones de sueldo"></a>
+              <h4>Liquidaciones</h4>
+              <div class="text-muted">Modulo 1</div>
+            </div>
+            <div class="col-6 col-sm-3 placeholder">
+              <img src="{{ asset('images/matriculas.png') }}" width="200" height="200" class="img-fluid rounded-circle" alt="Administración de matrículas">
+              <h4>Matrículas</h4>
+              <span class="text-muted">Modulo 2</span>
+            </div>
+            <div class="col-6 col-sm-3 placeholder">
+              <img src="{{ asset('images/rrhh.png') }}" width="200" height="200" class="img-fluid rounded-circle" alt="Administración de recursos humanos">
+              <h4>RR.HH</h4>
+              <span class="text-muted">Modulo 3</span>
+            </div>
+            <div class="col-6 col-sm-3 placeholder">
+              <img src="{{ asset('images/notas.png') }}" width="200" height="200" class="img-fluid rounded-circle" alt="Administración notas alumnado">
+              <h4>Notas</h4>
+              <span class="text-muted">Modulo 4</span>
+            </div>
+          </section>
+        @show
+        @yield('content')
+        </main>
+      
+     
+    
+      </div>
+
+
+    </div>
+          
+          
+         
+
+        
+  
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../../public/js/jquery.min.js"><\/script>')</script>
+    <script src="{{ asset('js/popper.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+ 
+</body>
+</html>
