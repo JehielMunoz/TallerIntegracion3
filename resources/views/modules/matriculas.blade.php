6e851ec055e375e51fa8253db05cb5a2116a04d1
Waldo Ancacoy <wancacoy2014@alu.uct.cl>
2017-10-02 20:08:04 -0300

    ...

--- resources/views/modules/matriculas.blade.php
+++ resources/views/modules/matriculas.blade.php
@@ -5,7 +5,8 @@
 @section('content')
 <style>
 	.div_deck_h{
-		height: 22em;
+		height: 23em;
+    margin-top: 5em;
 	}
 	.i_mgn_top{
 		margin-top: 0.7em;
@@ -15,13 +16,13 @@
 	}
 	.btn_mgn_top{
 		margin-top: 1.5em;
-	}
+  }
 </style>
 <div class="card-deck div_deck_h">
   <div class="card text-center">
   	<i class="fa fa-book fa-5x i_mgn_top" aria-hidden="true"></i>
     <div class="card-block div_mgn_top">
-      <h4 class="card-title">Formulario de Inscripción de Alumno</h4>
+      <h4 class="card-title">Formulario de Inscripción de Alumnos</h4>
       <a href="{{ url('formulario_matricula') }}" class="btn btn-outline-info btn_mgn_top" role="button">Ir a Formulario</a>
     </div>
   </div>
