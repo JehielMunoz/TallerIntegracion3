a7788d884bcf739c6ee390822df6aa16f94bbb7a
Waldo Ancacoy <wancacoy2014@alu.uct.cl>
2017-10-25 10:45:33 -0300

    new delete

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ Route::get('/', function () {
 });
 
 Route::get('/liquidaciones','HomeController@liquidaciones');
+Route::get('/liquidaciones/agregar','HomeController@liquidaciones_agregar');
 Route::get('/matriculas','HomeController@matriculas');
 Route::get('/matriculas/agregar','HomeController@matriculas_agregar');
 Route::get('/recursos-humanos','HomeController@recursos_humanos');
@@ -31,12 +32,17 @@ Route::get('/home', 'HomeController@index')->name('home');
     Liquidaciones de sueldo 
 */
 Route::get('autocompletar', ['as' => 'autocompletar', 'uses'=>'Busqueda_personal@Autocompletar']); // BuscarPersonal Autocompletado
-Route::post('BuscarEmpleado',['as'=> 'BuscarEmpleado', 'uses'=>'Busqueda_personal@CargarEmpleado']);
+Route::post('BuscarEmpleado',['as'=> 'BuscarEmpleado', 'uses'=>'Busqueda_personal@CargarEmpleado']); // Carga datos del empleado cargado
+Route::get('BorrarDato',['as'=> 'BorrarDatos', 'uses'=>'Busqueda_personal@BorrarDatos']); // Maneja cuando se borra una gratificacion o descuento entre otras cosas
+Route::get('AgregarDato',['as'=> 'AgregarDato', 'uses'=>'Busqueda_personal@AgregarDatos']); // Maneja cuando se agrega una gratificacion o descuento entre otras cosas
+Route::get('ModificarDato',['as'=> 'ModificarDatos', 'uses'=>'Busqueda_personal@ModificarDatos']); // Maneja cuando se agrega una gratificacion o descuento entre otras cosas
+Route::post('agregar_empleado',['as'=> 'agregar_empleado', 'uses'=>'Busqueda_personal@agregar_empleado']);
+
+
 
 Route::get('autocompletar_alumno', ['as' => 'autocompletar_alumno', 'uses'=>'Busqueda_estudiante@Autocompletar']); 
 Route::post('BuscarAlumno',['as'=> 'BuscarAlumno', 'uses'=>'Busqueda_estudiante@CargarAlumno']);
-
-
 Route::post('agregar_alumno',['as'=> 'agregar_alumno', 'uses'=>'Busqueda_estudiante@agregar_alumno']);
-
+Route::get('ModificarDatosAlumno',['as'=> 'ModificarDatosAlumno', 'uses'=>'Busqueda_estudiante@ModificarDatos']);
 Route::get('eliminar_alumno', ['as' => 'eliminar_alumno', 'uses'=>'Busqueda_estudiante@eliminar_alumno']); 
+
