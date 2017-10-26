a0abd54b86e9fe6baeaf6dc94d107e274a126798
Waldo Ancacoy <wancacoy2014@alu.uct.cl>
2017-10-26 11:45:58 -0300

    elieima

--- resources/views/modules/matriculas/noAlumno.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="jumbotron jumbotron-fluid">
-                                <div class="container">
-                                    <h1 class="display-3">Debe buscar un alumno</h1>
-                                    <hr class="my-2">
-                                    <h2> Para visualizar el contenido de esta pagina, debe buscar a un alumno primero </h2>
-                                </div>
-                            </div>
