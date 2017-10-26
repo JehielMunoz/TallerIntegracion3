d2427b59e283cc6243b829f2998a1e0b079d69ac
Waldo Ancacoy <wancacoy2014@alu.uct.cl>
2017-10-26 11:47:35 -0300

    elieima

--- resources/views/modules/matriculas/agregar/ant_familiares.blade.php
+++
@@ -1,8 +0,0 @@
-<?php
- use App\Http\Controllers\Busqueda_estudiante;
-?>
-<div class="table-responsive">
-    <table class="table">
-        {{Busqueda_estudiante::Datos_familiares()}}
-    </table>
-</div>
