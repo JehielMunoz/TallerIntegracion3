f390837bcd7595361744af32ea0412699041536a
Alexmzc <michelmz90@hotmail.com>
2017-11-22 04:19:24 -0300

    vista previa funcionando
    tiene detalles, faltan puntos y los descuentos por licencias.

--- public/js/formatoNumeros.js
+++ public/js/formatoNumeros.js
@@ -4,6 +4,12 @@ $(document).ready(Principal);
 function Principal(){
 	formatearDinero();
 	formatearRut();
+<<<<<<< HEAD
+    //formatearDineroV();
+    formatearRutV();
+    //console.log($("#formatoDinerov").attr('value')[0]);
+=======
+>>>>>>> 0636651b8425d00814f07e848defbfd3ec0acc69
 }
 
 function formatearDinero(){
@@ -17,6 +23,24 @@ function formatearDinero(){
         }
     }
 }
+<<<<<<< HEAD
+
+function formatearDineroV(){ //para vista previa
+    //var aDineros = []
+    aDineros = $('[id="formatoDineroV"]');
+    
+    
+    console.log(aDineros[0].innerHTML);
+    for(var i=0; i<aDineros.length; i+=1){
+        aDineros[i].innerHTML = "$"+aDineros[i].innerHTML.trim();
+
+    }
+    
+
+}
+
+=======
+>>>>>>> 0636651b8425d00814f07e848defbfd3ec0acc69
 function formatearRut() {
     rut = $("#Ruta");
     //console.log(rut[0].value.length);
@@ -30,3 +54,23 @@ function formatearRut() {
     rut[0].value = rut[0].value.split('').reverse().join('').replace(/^[\.]/,'') + "-"+ dv;   
 }
 
+<<<<<<< HEAD
+function formatearRutV(){
+    arut = $("[id='Rutv']");
+    console.log(arut);
+    for(var i=0; i<arut.length;i+=1){
+        console.log(typeof(arut[i].innerHTML));
+        if(arut[i].innerHTML[0] == "0"){
+            arut[i].innerHTML = arut[i].innerHTML.substring(1,arut[i].innerHTML.length);
+        }
+
+        dv = arut[i].innerHTML[arut[i].innerHTML.length-1]
+        arut[i].innerHTML = arut[i].innerHTML.substring(0,arut[i].innerHTML.length-1)
+        arut[i].innerHTML = arut[i].innerHTML.replace(/\./g,'');
+        arut[i].innerHTML = arut[i].innerHTML.toString().split('').reverse().join('').replace(/(?=\d*\.?)(\d{3})/g,'$1.');
+        arut[i].innerHTML = arut[i].innerHTML.split('').reverse().join('').replace(/^[\.]/,'') + "-"+ dv;
+    }
+
+}
+=======
+>>>>>>> 0636651b8425d00814f07e848defbfd3ec0acc69
